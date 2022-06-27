adc761e8457a707839e1b26243d634aa2365c1c9
makz <mr.greenmousth@gmail.com>
2022-06-27 15:41:23 -0400

    day 29

--- 09.intermediate_algorithm_scripting/13.sum_all_primes.js
+++ 09.intermediate_algorithm_scripting/13.sum_all_primes.js
@@ -1,13 +1,35 @@
+
 // A prime number is a whole number greater than 1 with exactly two divisors: 1 and itself. 
 // For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 4 is 
 // not prime since it is divisible by 1, 2 and 4.
 
 // Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
 
-// n is prime if n < 1 && n % 1 === 0 && n % n === 0 && n % x !=== 0 / where x is any other number rather than 1 or n
+/* TODO: improve this */
+const isPrime = n => {
+    for (let i = 2; i <= n; i++) {
+        if (!(i === n || n % i !== 0)) {
+            return false;
+        }
+    }
+    return true;
+};
 
 function sumPrimes(num) {
+    let sum = 0;
+    for (let i = 2; i <= num; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+
     return sum;
 }
 
-console.log(sumPrimes(10));
+console.log(sumPrimes(977));
+
+// sumPrimes(10) should return a number.
+// sumPrimes(10) should return 17.
+// sumPrimes(977) should return 73156.
+
+// primes : 2, 3, 5, 7, 11, 13, 17, 19, 23 and 29.
--- 09.intermediate_algorithm_scripting/16.steam_roller.js
+++ 09.intermediate_algorithm_scripting/16.steam_roller.js
@@ -1,6 +1,15 @@
 // Flatten a nested array. You must account for varying levels of nesting.
 
 function steamrollArray(arr) {
+    return arr.reduce((a, el) => Array.isArray(el) ? a.concat(...el) : a.concat(el), []);
 }
 
-console.log(steamrollArray([1, [2], [3, [[4]]]]));
+// console.log(steamrollArray([1, [2], [3, [[4]]]]));
+// console.log(steamrollArray([[["a"]], [["b"]]]));
+console.log(steamrollArray([1, [], [3, [[4]]]]));
+
+// steamrollArray([[["a"]], [["b"]]]) should return ["a", "b"].
+// steamrollArray([1, [2], [3, [[4]]]]) should return [1, 2, 3, 4].
+// steamrollArray([1, [], [3, [[4]]]]) should return [1, 3, 4].
+// steamrollArray([1, {}, [3, [[4]]]]) should return [1, {}, 3, 4].
+// Your solution should not use the Array.prototype.flat() or Array.prototype.flatMap() methods.
