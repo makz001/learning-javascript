6a9d2fae6d3774173b89d2aa7e45ce92c87972fe
makz <mr.greenmousth@gmail.com>
2022-06-27 22:24:20 -0400

    day 30

---
+++ 10.javascript_algorithms_and_data_structures_projects/03.caesars_cipher.js
@@ -0,0 +1,38 @@
+// One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift 
+// cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 
+// 13 places. Thus A ↔ N, B ↔ O and so on.
+
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+// All letters will be uppercase. Do not transform any non-alphabetic 
+// character (i.e. spaces, punctuation), but do pass them on.
+
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+function rot13(str) {
+    let regex =/\w/;
+    let ss = '';
+
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i];
+        let index = alphabet.indexOf(letter);
+
+        if (regex.test(letter)) {
+            index = index + 13 >= alphabet.length ? index + 13 - alphabet.length : index + 13
+            ss += alphabet[index];
+        } else {
+            ss += letter;
+        }
+
+    }
+    return ss;
+}
+
+rot13("SERR PBQR PNZC");
+
+// rot13("SERR PBQR PNZC") should decode to the string FREE CODE CAMP
+// rot13("SERR CVMMN!") should decode to the string FREE PIZZA!
+// rot13("SERR YBIR?") should decode to the string FREE LOVE?
+// rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to the string THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
